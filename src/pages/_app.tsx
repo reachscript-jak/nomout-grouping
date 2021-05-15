20d2f907d686e097393e88ae5353a273d9ee0588
kento-kuga <kento.kuga.hidari@gmail.com>
2021-05-15 19:05:41 +0900

    chore : Recoilの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,16 @@ import "src/styles/globals.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { RecoilRoot } from "recoil";
 
 const App = (props: AppProps) => {
   return (
-    <>
+    <RecoilRoot>
       <Head>
-        <title>nexst</title>
+        <title>nomount-grouping</title>
       </Head>
       <props.Component {...props.pageProps} />
-    </>
+    </RecoilRoot>
   );
 };
 
