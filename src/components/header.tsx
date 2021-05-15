4d0ecc313b872d0f76054e39193fac77da4055f5
kento-kuga <kento.kuga.hidari@gmail.com>
2021-05-15 19:12:10 +0900

    feat : 画面雛形の作成

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,12 @@
-import Link from "next/link";
+import React from "react";
 
-const items = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-];
-
-export const Header = () => {
+type Props = {};
+const Header: React.VFC<Props> = () => {
   return (
-    <header>
-      <h1>Title</h1>
-      <nav>
-        {items.map(({ href, label }) => {
-          return (
-            <Link key={href} href={href}>
-              <a style={{ display: "inline-block", padding: 12 }}>{label}</a>
-            </Link>
-          );
-        })}
-      </nav>
+    <header className="h-24">
+      <span className=" border-b-2 border-black inline-block pt-5 pl-7 pb-2 text-7xl">Grouping</span>
     </header>
   );
 };
+
+export default Header;
--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,18 +1,18 @@
+import React from "react";
 import Container from "src/components/container";
 import Header from "src/components/header";
 
-const Home: React.VFC = () => {
+const Result: React.VFC = () => {
   return (
     <>
       <Header />
       <main>
         <Container>
-          <h1>メイン画面</h1>
+          <h1>結果画面</h1>
         </Container>
       </main>
     </>
   );
 };
 
-// eslint-disable-next-line import/no-default-export
-export default Home;
+export default Result;
