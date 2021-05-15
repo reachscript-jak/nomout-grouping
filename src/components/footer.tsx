af99d41019d64e016a649faf50a2aa1f30bb3016
kento-kuga <kento.kuga.hidari@gmail.com>
2021-05-15 19:11:12 +0900

    refactor : 不要ファイルの削除

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer>
-      <small>&copy; 20xx example</small>
-    </footer>
-  );
-};
--- src/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-import { Layout } from "src/components/layout";
-
-const About = () => {
-  return <Layout>About</Layout>;
-};
-
-// eslint-disable-next-line import/no-default-export
-export default About;
