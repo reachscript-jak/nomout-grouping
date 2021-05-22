0700155e3d8e67e55c4a64495ea12aa919d1ef65
kento-kuga <kento.kuga.hidari@gmail.com>
2021-05-22 18:49:50 +0900

    feat : タイトルの大きさを調整

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@
 type Props = {};
 const Header: React.VFC<Props> = () => {
   return (
-    <header className="h-24">
-      <span className=" border-b-2 border-black inline-block pt-5 pl-7 pb-2 text-7xl">Grouping</span>
+    <header className="h-24 font-bold">
+      <span className=" border-b-2 border-black inline-block pt-5 pl-7 pb-2 text-5xl">Grouping</span>
     </header>
   );
 };
