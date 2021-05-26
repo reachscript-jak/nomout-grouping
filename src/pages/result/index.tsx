32d1f79e8d52d4ea5916fe058e7d2c1c77c335de
mamezou <mamezou.king@gmail.com>
2021-05-26 21:29:01 +0900

    feat : 結果の表示機能とgroupsがないときはトップへ戻るように設定

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,10 +1,20 @@
+import { useRouter } from "next/dist/client/router";
+import { useEffect, useState } from "react";
 import Container from "src/components/container";
 import Header from "src/components/header";
+import { useGroups } from "src/hooks/use-groups";
 import { useMembers } from "src/hooks/use-members";
 
 const Result: React.VFC = () => {
   // hooks
+  const [groupNumber] = useState(3);
   const { members } = useMembers();
+  const { groups, setGroups } = useGroups();
+  const router = useRouter();
+
+  useEffect(() => {
+    groups.length == 0 && router.push("");
+  }, []);
 
   return (
     <>
@@ -12,12 +22,26 @@ const Result: React.VFC = () => {
       <main>
         <Container>
           <h1>結果画面</h1>
-          <ul>
-            {members.map((member, i) => {
-              return <li key={i + member.name}>{`ID : ${member.mid} Name: ${member.name}`}</li>;
-            })}
-          </ul>
-          <button className="... ring-0 focus:outline-none">やり直す</button>
+          {groups.map((num, i) => {
+            return (
+              <div key={"グループ" + i}>
+                <h2>{`グループ${i + 1}`}</h2>
+                <ul>
+                  {num.members.map((people, j) => {
+                    return <li key={j + people.name}>{`ID：${people.mid}名前：${people.name}`}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+          <button
+            className="... ring-0 focus:outline-none"
+            onClick={() => {
+              setGroups(members, groupNumber);
+            }}
+          >
+            やり直す
+          </button>
         </Container>
       </main>
     </>
